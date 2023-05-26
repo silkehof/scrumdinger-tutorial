25f440ff2f002ac34f759dcdcacfdd89dfad7101
Silke Hofmann <silke@silkehofmann.eu>
2023-05-26 22:46:27 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Silke Hofmann on 26.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
