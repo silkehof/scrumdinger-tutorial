e0cef953b3aa1b4a70bd6e7cf76943702ed643f9
Silke Hofmann <silke@silkehofmann.eu>
2023-05-28 01:43:22 +0200

    iOS App Dev Tutorials - Scrumdinger | Navigation and modal presentation | Add DetailView

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Silke Hofmann on 28.05.23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            DetailView(scrum: DailyScrum.sampleData[0])
+        }
+    }
+}
