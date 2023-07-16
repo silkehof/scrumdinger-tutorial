28db50ff17e88d909cbd3af8b812ba4c1fd9bccd
Silke Hofmann <silke@silkehofmann.eu>
2023-07-17 00:31:55 +0200

    Play sound when speaker's turn is over

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -31,6 +31,10 @@ struct MeetingView: View {
         .foregroundColor(scrum.theme.accentColor)
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
             scrumTimer.startScrum()
         }
         .onDisappear {
